93f3176bf3fb0c6bfe06c24f73eaa1893d904330
Valery <vlazurenko9@gmail.com>
2022-09-28 13:44:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+/*let incr = 10,
+decr = 10;
+//++incr; 
+//--decr;
+//console.log(++incr);
+console.log(5%2);
+console.log(4*2 != "8");
+const b = false;
+const c = false;
+console.log(b || !c);*/
+let a = 1, 
+b = 2;
+
+     if (a > b)  {console.log("Yes ")}
+else  {console.log("No")}
+   
+  
